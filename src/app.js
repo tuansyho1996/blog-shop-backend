e0eecfda9b3f41c367cc331abea39cb646d08a94
tuansyho <hosytuan101296@gmail.com>
2025-05-11 14:51:13 +0700

    auto sync

--- src/app.js
+++ src/app.js
@@ -10,6 +10,30 @@ const app = express();
 // app.use(cookieParser())
 
 // init middleware
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH,OPTIONS');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,Authorization,X-Client-Id');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
 app.use(express.json())
